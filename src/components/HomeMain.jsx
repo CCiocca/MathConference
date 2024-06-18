5d411870058308982c0340efc5a9feb04c9f2445
CCiocca <chiara.cioccarelli1@gmail.com>
2024-06-18 19:23:33 +0200

    homepage upd

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -3,9 +3,26 @@ import React from 'react'
 const HomeMain = () => {
   return (
     <div className='home-main-container'>
-        <p>The main theme of this workshop is Enumerative Geometry, with particular emphasis on the geometry of moduli spaces and their refined invariants. The workshop consists in several plenary talks given by international experts, and aims to foster the collaboration among researchers interested in Enumerative Geometry in a broad sense.</p>
-        <p>There will be a special journal section in RIMUT (Rendiconti dell’Istituto di Matematica dell’Università di Trieste. An International Journal of Mathematics), dedicated to the themes of the conference. Everyone is welcome to contribute (submissions go through the usual refereeing system, see https://rendiconti.dmi.units.it). The deadline for paper submissions is January 31, 2024.</p>
-        
+        <div className="intro">
+            <p>The main theme of this workshop is Enumerative Geometry, with particular emphasis on the geometry of moduli spaces and their refined invariants. The workshop consists in several plenary talks given by international experts, and aims to foster the collaboration among researchers interested in Enumerative Geometry in a broad sense.</p>
+            <p>There will be a special journal section in RIMUT (Rendiconti dell’Istituto di Matematica dell’Università di Trieste. An International Journal of Mathematics), dedicated to the themes of the conference. Everyone is welcome to contribute (submissions go through the usual refereeing system, see https://rendiconti.dmi.units.it). The deadline for paper submissions is January 31, 2024.</p>
+        </div>
+        <div className="speakers">
+            <h2>Speakers</h2>
+
+        </div>
+        <div className="organizers">
+            <h2>Organizing Committee</h2>
+            
+        </div>
+        <div className="sponsors">
+            <h2>Sponsors</h2>
+            <div className="sponsor-img">
+                <img src="" alt="" />
+            </div>
+            
+        </div>
+
     </div>
   )
 }
