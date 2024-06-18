5d19c5bc20ba179b23bd618d8840879917183f45
CCiocca <chiara.cioccarelli1@gmail.com>
2024-06-18 19:19:40 +0200

    first commit

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const HomeMain = () => {
   return (
     <div className='home-main-container'>
-        <p></p>
+        <p>The main theme of this workshop is Enumerative Geometry, with particular emphasis on the geometry of moduli spaces and their refined invariants. The workshop consists in several plenary talks given by international experts, and aims to foster the collaboration among researchers interested in Enumerative Geometry in a broad sense.</p>
+        <p>There will be a special journal section in RIMUT (Rendiconti dell’Istituto di Matematica dell’Università di Trieste. An International Journal of Mathematics), dedicated to the themes of the conference. Everyone is welcome to contribute (submissions go through the usual refereeing system, see https://rendiconti.dmi.units.it). The deadline for paper submissions is January 31, 2024.</p>
+        
     </div>
   )
 }
